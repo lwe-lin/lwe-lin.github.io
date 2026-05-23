b7dbe9cc610b8226f75a23fb9b18cb1146c4ad9c
GH15339 <why20231127@gmail.com>
2026-05-23 15:14:08 +0800

    新增chess-game網站的靜態儲存空間

---
+++ chess-game/build/WebAssembly/pkg/executor_wasm_bg.wasm.d.ts
@@ -0,0 +1,28 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const main: () => void;
+export const __wbg_outputdevice_free: (a: number, b: number) => void;
+export const outputdevice_close: (a: number) => void;
+export const wasm_bindgen__convert__closures_____invoke__h94706bdaa0c45fc4: (a: number, b: number, c: any) => [number, number];
+export const wasm_bindgen__convert__closures_____invoke__h79f5a7193862a5a1: (a: number, b: number, c: any, d: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h92574804bddc2e4b: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h92574804bddc2e4b_3: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h92574804bddc2e4b_4: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h92574804bddc2e4b_5: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h92574804bddc2e4b_6: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h92574804bddc2e4b_7: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h92574804bddc2e4b_8: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h92574804bddc2e4b_9: (a: number, b: number, c: any) => void;
+export const wasm_bindgen__convert__closures_____invoke__h9b0b56b5a754a74b: (a: number, b: number) => void;
+export const wasm_bindgen__convert__closures_____invoke__h58ec6da0198e8106: (a: number, b: number) => void;
+export const wasm_bindgen__convert__closures_____invoke__h4b48ba6facb83c43: (a: number, b: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_destroy_closure: (a: number, b: number) => void;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_start: () => void;
